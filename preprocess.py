46b26ce01a6c16838e824726b6e73d0d8345cd2d
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-17 00:36:51 +0530

    Refactor buzzword lists and ensure proper formatting in preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -3,3 +3,7 @@ import tensorflow as tf
 import clip_wrapper as cw
 import glob
 from pycocotools.coco import COCO
+
+small_buzzwords = ["cow", "sheep"]
+large_buzzwords = ["cow", "sheep", "mountain", "hill", "countryside", "grass", "forest", "nature", 
+                 "farm", "alpacca", "horse", "landscape", "fence"]
