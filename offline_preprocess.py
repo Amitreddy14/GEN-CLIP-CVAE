50f3ee3a8f6bd43fbeb18cab6a2d023c85ad2003
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-17 11:15:42 +0530

    Fix formatting of offline_preprocess.py

--- offline_preprocess.py
+++ offline_preprocess.py
@@ -3,3 +3,6 @@ os.environ['KMP_DUPLICATE_LIB_OK']='True'
 # import preprocess as pp
 from pycocotools.coco import COCO
 import shutil
+
+buzzwords = ["cow", "sheep", "mountain", "hill", "countryside", "grass", "forest", "nature", 
+                 "farm", "alpaca", "horse", "landscape", "fence"]
