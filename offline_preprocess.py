ab59e6ca6f5a9e2d5cc2da12811ef980f4df004c
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-17 11:29:14 +0530

    Refactor check_naturey function to improve buzzword matching logic

--- offline_preprocess.py
+++ offline_preprocess.py
@@ -6,3 +6,10 @@ import shutil
 
 buzzwords = ["cow", "sheep", "mountain", "hill", "countryside", "grass", "forest", "nature", 
                  "farm", "alpaca", "horse", "landscape", "fence"]
+
+def check_naturey(captions: list[str]):
+    #returns true if a caption for this image has one of our buzz words, false otherwise
+    def match(word):
+        for capt in captions:
+            if word in capt.lower():
+                return True
