5df17a2948b84ecb5ad08786bcd5cde3da60e987
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-17 12:04:07 +0530

    Add constructor to ClipCVAE class for initializing model parameters

--- model_64x64.py
+++ model_64x64.py
@@ -8,3 +8,9 @@ import clip_wrapper
 # Please check the Final Notebooks for the most up-to-date, accurate model
 BATCH_SIZE = 16
 class ClipCVAE(tf.keras.Model):
+
+    def __init__(self, input_shape,  latent_dim, dropout_rate):
+    super(ClipCVAE, self).__init__()
+    self.latent_dim = latent_dim
+    self.shape_input = input_shape
+    self.dropout_prob = dropout_rate
