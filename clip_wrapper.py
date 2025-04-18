7303ff271fb0e0b716ce566d4300ee3d390c91e4
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-19 00:14:36 +0530

    Fix formatting by adding newlines at the end of the get_text_encoding function

--- clip_wrapper.py
+++ clip_wrapper.py
@@ -21,3 +21,9 @@ def get_text_encoding(tokens: list[str]):
     with torch.no_grad():
         text_features = model.encode_text(text)
         return text_features.cpu().numpy()
+    
+def batch_get_image_encodings(images: tf.Tensor):
+    torch_tensor = torch.from_numpy(images.cpu().numpy()).permute(0, 3, 1, 2).to(device) # Change from [N, H, W, C] to [N, C, H, W] (different convention)
+    with torch.no_grad():
+        image_features = model.encode_image(torch_tensor)
+        return image_features.cpu().numpy()
