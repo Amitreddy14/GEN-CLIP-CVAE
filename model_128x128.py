53c1f6090f618730ea353826c66a981dda94768d
Amit Reddy <jsammureddy140804@gmail.com>
2025-04-19 01:03:00 +0530

    Fix formatting by adding newlines at the end of the model_128x128.py file

--- model_128x128.py
+++ model_128x128.py
@@ -3,3 +3,10 @@ import torch
 import os
 import matplotlib.pyplot as plt
 import clip_wrapper
+
+class ClipCVAE(tf.keras.Model):
+  def __init__(self, input_shape,  latent_dim, dropout_rate):
+    super(ClipCVAE, self).__init__()
+    self.latent_dim = latent_dim
+    self.shape_input = input_shape
+    self.dropout_prob = dropout_rate
